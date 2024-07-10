b771d12c2b6e2ffe0907b8bca633a46e18420f20
Diti2604 <ditiboyboy@gmail.com>
2024-07-10 18:30:49 +0200

    until 15 done

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function BlogList({blogs, title}) {
-    // const blogs = props.blogs;
-    // const title = props.title;
-    // console.log(props, blogs);
+function BlogList({ blogs, title, handleDelete }) {
+  // const blogs = props.blogs;
+  // const title = props.title;
+  // console.log(props, blogs);
+
   return (
-    <div className='blog-list'>
-        <h2>{title}</h2>
-       {blogs.map((blog) => (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}> Delete blog</button>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default BlogList
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 function Home() {
@@ -12,13 +12,24 @@ function Home() {
       id: 3,
     },
   ]);
+
+  const [name, setName] = useState("mario");
+
+  function handleDelete(id) {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's Blogs"
-      />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("Luigi")}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 }
