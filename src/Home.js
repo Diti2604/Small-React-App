86a7e7812c7fe62b01e849ec5fbab31f86b35bce
Diti2604 <ditiboyboy@gmail.com>
2024-07-10 12:53:30 +0200

    7&8 done

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function Home() {
+    const handleClick = () =>{
+        console.log('hello, ninjas');
+    }
+
+    const handleClickAgain = (name) =>{
+        console.log('hello ' + name);
+    }   
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("Mario")}>Click me again</button>
     </div>
   )
 }
